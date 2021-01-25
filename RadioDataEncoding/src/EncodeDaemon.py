e9e21a8ab0db48f614c29cdcf64b64e69f8d739c
Murphyj1098 <jrmurphy@wpi.edu>
2021-01-25 13:11:29 -0500

    <feat> Daemon control's upload and GNURadio

--- RadioDataEncoding/src/EncodeDaemon.py
+++ RadioDataEncoding/src/EncodeDaemon.py
@@ -1,22 +1,74 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-# Imports
+## Imports
 import daemon           # Creating daemon process (non-standard lib)
-import daemon.pidfile   # Creating daemon pid file 
+import daemon.pidfile   # Creating daemon pid file
 import signal           # Kernel signal handling
+import threading        # Seperate thread for GNURadio and EncodeTransfer
+import time             # Program Sleep
+
+from top_block import (
+    top_block
+) # GNURadio top block class
 
 from EncodeTransfer import (
-    term,
-    main
-) # Transfer script
+    main as EncodeMain
+) # Transfer function
 
-# Constants
-WORKDIR = '/home/radios/SpectrumObservatory-Radios/RadioDataEncoding/src'
-LOG = WORKDIR + '/WSDaemon.log'
-PID = WORKDIR + '/WSDaemon.pid'
 
-if __name__ == "__main__":
-    logFile = open(LOG, 'w+')   # Open log file
+## Constants
+WORKDIR = '/home/jrmurphy/dev/MQP/SpectrumObservatory-Radios/RadioDataEncoding/src'
+LOG = WORKDIR + '/RadioDaemon.log'
+PID = WORKDIR + '/RadioDaemon.pid'
+
+
+## GNURadio job class
+class GNURadioJob(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+        self.shutdown_flag = threading.Event()
+
+    def run(self):
+        tb = top_block()
+        tb.start()
+
+        # Spin until shutdown is signaled
+        while not self.shutdown_flag.is_set():
+            time.sleep(0.5)
+
+        tb.stop()
+        tb.wait()
+
+
+## Encode and Transfer script job class
+class EncodeTransferJob(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+        self.shutdown_flag = threading.Event()
+
+    def run(self):
+        EncodeMain()
+
+        # Spin until shutdown is signaled
+        while not self.shutdown_flag.is_set():
+            time.sleep(0.5)
+
+
+## Declare our custom exception
+class ServiceExit(Exception):
+    pass
+
+
+## Call our custom exception when a job's shutdown flag is set by a SIGTERM
+def service_shutdown(signum, frame):
+    raise ServiceExit
+
+
+## Main thread
+def main():
+    logFile = open(LOG, 'w+') # Open log file
 
     # Set up deamon context (process working dir and std i/o)
     context = daemon.DaemonContext(
@@ -28,11 +80,40 @@ if __name__ == "__main__":
 
     # Set up signal interupt handlers
     context.signal_map = {
-        signal.SIGTERM: term,
-        signal.SIGHUP: None,
+        signal.SIGTERM: service_shutdown
     }
 
-    
+    # Set up threads
+    t1 = GNURadioJob()
+    t2 = EncodeTransferJob()
+
     with context:
-        # main program loop
-        main()
+        # Start the job threads
+        try:
+            print("\nStart GNURadio\n")
+            t1.start()
+
+            time.sleep(5)   # NOTE: need to wait for GNURadio to finish setting up the radio
+                            # Can GNURadio signal this? This time is variable depending of if FPGA needs to be flashed
+
+            print("\nStart Upload Script\n")
+            t2.start()
+ 
+            # Keep the main thread running, otherwise signals are ignored.
+            while True:
+                time.sleep(0.5)
+ 
+        except ServiceExit:
+            # Terminate the running threads.
+            # Signal the GNURadio thread to stop
+            t1.shutdown_flag.set()
+            t2.shutdown_flag.set()
+
+            # Wait for the threads to close...
+            t1.join()
+            t2.join()
+ 
+    print('Exiting main program')
+
+if __name__ == "__main__":
+    main()
--- RadioDataEncoding/src/EncodeTransfer.py
+++ RadioDataEncoding/src/EncodeTransfer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 ## Imports
 import base64   # Encoding binary data
@@ -13,27 +13,20 @@ from gnuradio.blocks import parse_file_metadata # GNURadio header parsing
 
 ## Constants
 HOST        = "http://spectrumobservatory.wpi.edu:5000/data1"               # host of webserver
-BINNAME     = "sample_header.dat"                                          # name of the input file
-HEADERS     = {'Content-type': 'application/json', 'Accept': 'text/plain'} # Headers for POST request
-NITEMS      = 1000000                                                      # Number of samples per header (match to GNURadio)
+BINNAME     = "sample.dat"                                                  # name of the input file
+HEADERS     = {'Content-type': 'application/json', 'Accept': 'text/plain'}  # Headers for POST request
+NITEMS      = 300000                                                        # Number of samples per header (match to GNURadio)
 
-# Test Constants -> (points to local test server)
-TEST = 0
+# Test Constants -> (can be used to test locally)
+TEST = 1
 if(TEST):
     HOST = 'http://localhost:3000/data'
-    BINNAME = 'sample_header.dat'
-
-
-## Called if the program recieves a SIGTERM signal # NOTE: Invoked by daemon
-# This handles any necessary cleanup
-def term(signum, frame):
-    print(f"[o] Signal number {signum} received")
-    print("[o] Terminating the program")
-    exit(1)
+    BINNAME = 'sample.dat'
 
 
 ## This will be called to parse header data out of the dat file
 # Takes in an open file descriptor and returns a python dictionary
+# NOTE: This function is built on the gr_read_file_metadata program from GNURadio
 def parseHeaders(file):
 
     # read out header bytes into a string
@@ -70,20 +63,20 @@ def main():
     ## Open the binary file
     inputFile = open(BINNAME, "rb")
 
-    ## Loop control variables
+    ## Initialize loop control variables
     isComplete = False
     headerNum = 0
 
     ## Main loop: 
     # 1. Read GNURadio output file
     # 2. Parse header info
-    # 3. Encode payload
-    # 4. Create JSON
-    # 5. Send JSON with HTTP POST
+    # 3. Check if final segment
+    # 4. Encode payload
+    # 5. Create JSON
+    # 6. Send JSON with HTTP POST
     while(not isComplete): # Loop until all headers and subsequent data is read
 
-        ## Read in bin file to parse header metadata
-        # NOTE: for now we just read in one segment of data
+        # Read in bin file to parse header metadata
         headerData = parseHeaders(inputFile)
         headerNum += 1
         print(f"Header Number: {headerNum}")
@@ -92,7 +85,7 @@ def main():
         ITEM_SIZE   = headerData["nitems"]
         SEG_SIZE    = headerData["nbytes"]
 
-        # Check if we read the final header
+        # Check if we read the final header in the file (if there are less samples than expected for a full segment)
         if ITEM_SIZE < NITEMS:
             isComplete = True
 
@@ -100,26 +93,29 @@ def main():
         rx_time     = headerData["rx_time"]
         rx_rate     = headerData["rx_rate"]
         num_samples = headerData["nitems"]
-        radio       = 1
+        radio       = 1 # TODO: set up custom header to indentify which radio data is from?
 
-        ## Encode data payload from bin file into base64 ascii characters
+        # Encode data payload from bin file into base64 ascii characters
         inputBinary = inputFile.read(SEG_SIZE)
         encodedData = (base64.b64encode(inputBinary)).decode('ascii')
 
-        ## Create JSON file using encoded payload and header metadata
+        # Create JSON file using encoded payload and header metadata
         jsonFormat = {"metadata":[{"rx_time" : rx_time, "rx_sample" : rx_rate, "num_samples" : num_samples, "radio_num" : radio}], "payload" : encodedData}
         jsonFile = json.dumps(jsonFormat, indent=4)
 
-        ## Send this JSON file to the WebServer with an HTTP POST
+        # Send this JSON file to the WebServer with an HTTP POST
         r = requests.post(url=HOST, data=jsonFile, headers=HEADERS)
         print("[+] Response from server: %s" %r)
-
-        ## Close, wait, and repeat process
-        rx_time = rx_time + 5   # temporary time header increment
         
-        # Wait to send next file (TODO: does this program need to do anything else in the meanwhile?)
-        print("[+] Sleeping for 5 seconds\n")
-        time.sleep(5) # TODO: sleep interval related to GNURadio file creation
+        # Wait to send next segment (segments are generated at a rate of 1 per second)
+        print("[+] Sleeping for 1 seconds\n")
+        time.sleep(1)
+
+
+    ## Any remaining cleanup
+    print("[o] Sent all data, cleaning up\n")
+    inputFile.close()
+    os.remove("sample.dat")
 
 if __name__ == "__main__":
     main()
